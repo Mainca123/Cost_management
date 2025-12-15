51bfc62609b6f2c1dcd60881414464d64e7260ed
mainca123 <giangchinhtgp@gmail.com>
2025-12-16 03:54:51 +0700

    fix port

--- src/main/java/com/example/HeThongQuanLyTaiChinhThongMinh/security/SecurityConfig.java
+++ src/main/java/com/example/HeThongQuanLyTaiChinhThongMinh/security/SecurityConfig.java
@@ -67,7 +67,7 @@ public class SecurityConfig {
                 "http://127.0.0.1:54110",
                 "http://127.0.0.1:5500",
                 "http://localhost:5174",
-                "https://hitnetwork.onrender.com"
+                "https://cost-management-na38.onrender.com"
         ));
         corsConfiguration.setAllowedHeaders(List.of("Origin", "Content-Type", "Accept", "Authorization"));
         corsConfiguration.setExposedHeaders(List.of("Authorization"));
